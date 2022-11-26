ce8f93282d8d499fab84d7ce4dde489aa89fddb7
Jose Garcia <jgcordoba@gmail.com>
2022-11-26 17:42:07 +0100

    0.1

--- Assets/Scripts/FailClick.cs
+++ Assets/Scripts/FailClick.cs
@@ -7,6 +7,10 @@ public class FailClick : MonoBehaviour
 {
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip failSound;
+    [SerializeField] GameObject pointClick;
+    [SerializeField] ParticleSystem failParticles;
+
+    Vector3 worldPosition;
 
 
     void Start()
@@ -17,14 +21,20 @@ public class FailClick : MonoBehaviour
  
     void Update()
     {
-        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitData;
+        if(Physics.Raycast(ray, out hitData, 1000))
+            {
+                worldPosition = hitData.point;
+            }
     
     }
 
     public void OnMouseDown()
     {
-        
-
         audioSource.PlayOneShot(failSound);
+        pointClick.transform.position = worldPosition;
+        failParticles.Play();
+       // Debug.Log(worldPosition);
     }
 }
