6230a802fdf95aa354c2317135f1e2e7a79d7b29
Jose Garcia <jgcordoba@gmail.com>
2022-11-26 11:38:58 +0100

    Anim

---
+++ Assets/Scripts/SpawnerTopos.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerTopos : MonoBehaviour
+{
+    public Vector3[] positionHoles;
+    public GameObject mole;
+
+
+    void Start()
+    {
+        Instantiate(mole, positionHoles[0], Quaternion.identity);
+        Instantiate(mole, positionHoles[1], Quaternion.identity);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
