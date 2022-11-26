fb4ed5940337176b312560eaead4660bbe465ba0
Jose Garcia <jgcordoba@gmail.com>
2022-11-26 17:05:51 +0100

    CollisionsRepair

--- Assets/Scripts/FailClick.cs
+++ Assets/Scripts/FailClick.cs
@@ -2,24 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class FailClick : MonoBehaviour
 {
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip failSound;
+
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+ 
     void Update()
     {
         
+    
     }
 
     public void OnMouseDown()
     {
-        //Debug.Log("Fail");
+        
+
         audioSource.PlayOneShot(failSound);
     }
 }
